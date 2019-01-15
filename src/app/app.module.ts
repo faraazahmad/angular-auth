3c470c8dfc092b0bea0c4337d36b0b5dd5c464bf
Syed Faraaz Ahmad <faraaz98@live.com>
2019-01-15 21:18:48 +0530

    button works on click

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from "@angular/forms";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
@@ -14,7 +16,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
@@ -9,10 +10,20 @@ export class AuthComponent implements OnInit {
 
   username: string;
   password: string;
+
+  show: boolean = false;
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.username + ' ' + this.password);
+  }
+
+  toggle() {
+    this.show = !this.show;
   }
 
 }
