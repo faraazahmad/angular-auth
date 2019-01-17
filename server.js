6b0cc1f3a7262fa0eecb18500c9f14af2167af2d
Syed Faraaz Ahmad <faraaz98@live.com>
2019-01-17 19:29:09 +0530

    add token verifier on backend

--- server.js
+++ server.js
@@ -26,6 +26,22 @@ function createToken() {
     return token;
 }
 
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized request.')
+    }
+    let token  = req.headers.authorization.split(' ')[1];
+    if (token === null) {
+        return res.status(401).send('Unauthorized request.')
+    }
+    let payload = jwt.verify(token, jwtSecret);
+    if (!payload) {
+        return res.status(401).send('Unauthorized request.')
+    }
+    req.username = payload.subject;
+    next();
+}
+
 // -------------------------------------------------------------------------------
 
 app.use(cors());
@@ -59,8 +75,8 @@ app.post("/login", (req, res) => {
     }
 });
 
-app.get("/resource", jwtVerifier({ secret: jwtSecret }), (req, res) => {
-    res.send("Where we\'re going, we don\'t need roads.");
+app.get("/resource", verifyToken, (req, res) => {
+    res.json("Where we\'re going, we don\'t need roads.");
 });
 
 app.listen(port, () => {
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../auth.service";
 import { Router } from "@angular/router";
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
@@ -23,7 +24,13 @@ export class AuthComponent implements OnInit {
         localStorage.setItem("token", res.token);
         this._router.navigate([ '/resource' ]);
       },
-      err => console.log(err)
+      err => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            this._router.navigate([ '/login' ]);
+          }
+        }
+      }
     );
   }
 
